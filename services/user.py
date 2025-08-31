943ce2115694b948d166f36252998e7b06e0bf10
yarik <super.racing@ukr.net>
2025-08-31 11:44:24 +0300

    get_user_model

--- services/user.py
+++ services/user.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
 
 def create_user(username: str,
@@ -17,7 +18,7 @@ def create_user(username: str,
     user.save()
 
 
-def get_user(user_id: int):
+def get_user(user_id: int) -> AbstractUser:
     return get_user_model().objects.get(id=user_id)
 
 
